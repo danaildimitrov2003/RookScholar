04f03421a8795efb0c9a20eced21f86b2adbe1c1
dvny <danaildimitrov2003@gmail.com>
2023-03-13 19:05:34 +0200

    remade the third screens constrains programmatically

--- RookScholar/ArticleDetailViewController.swift
+++ RookScholar/ArticleDetailViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SideMenu
 
-class ArticleDetailViewController: UIViewController, MenuControllerDelegate, DetailMenuControllerDelegate{
+class ArticleDetailViewController: UIViewController, DetailMenuControllerDelegate{
     
     
     private var sideMenu: SideMenuNavigationController?
@@ -18,28 +18,70 @@ class ArticleDetailViewController: UIViewController, MenuControllerDelegate, Det
     
     let articleTitle : UILabel = {
         let articleTitle = UILabel()
-        articleTitle .translatesAutoresizingMaskIntoConstraints = false // enable auto-layout
-        articleTitle .textAlignment = .center // text alignment in center
+        articleTitle.translatesAutoresizingMaskIntoConstraints = false
+        articleTitle.textAlignment = .center
+        articleTitle.font = articleTitle.font.withSize(27)
         return articleTitle
     }()
     let articleInfo : UILabel = {
         let articleInfo = UILabel()
-        articleInfo .translatesAutoresizingMaskIntoConstraints = false // enable auto-layout
-        articleInfo .textAlignment = .center // text alignment in center
+        articleInfo.translatesAutoresizingMaskIntoConstraints = false
+        articleInfo.textAlignment = .center
         return articleInfo
     }()
     let articleContent : UITextView = {
         let articleContent = UITextView()
-        articleContent .translatesAutoresizingMaskIntoConstraints = false // enable auto-layout
-        articleContent .textAlignment = .center // text alignment in center
+        articleContent.translatesAutoresizingMaskIntoConstraints = false
+        articleContent.textAlignment = .center
+        articleContent.isScrollEnabled = false
+        articleContent.isEditable = false
         return articleContent
     }()
     let articleImage : UIImageView = {
         let articleImage = UIImageView()
-        articleImage .translatesAutoresizingMaskIntoConstraints = false // enable auto-layout
+        articleImage.contentMode = UIView.ContentMode.scaleAspectFill
+        articleImage.translatesAutoresizingMaskIntoConstraints = false
+        //articleImage.frame = CGRect(x: 47, y: 191, width: 140, height: 214)
         return articleImage
     }()
-    
+    private func addLabels() {
+        view.addSubview(articleTitle)
+        view.addSubview(articleInfo)
+        view.addSubview(articleContent)
+        view.addSubview(articleImage)
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(articleTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor))
+        constraints.append(articleTitle.topAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10))
+        
+        constraints.append(articleInfo.topAnchor.constraint(
+            equalTo: articleTitle.topAnchor, constant: 30))
+        constraints.append(articleInfo.centerXAnchor.constraint(equalTo: view.centerXAnchor))
+        constraints.append(articleInfo.leadingAnchor.constraint(
+            equalTo: view.leadingAnchor, constant: 10))
+        constraints.append(articleInfo.trailingAnchor.constraint(
+            equalTo: view.trailingAnchor, constant: -10))
+        
+        
+        constraints.append(articleContent.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80))
+        constraints.append(articleContent.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10))
+        constraints.append(articleContent.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10))
+        
+        constraints.append(articleImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80))
+        constraints.append(articleImage.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10))
+        //constraints.append(articleImage.heightAnchor.constraint(equalTo: view.heightAnchor, constant: 107))
+        //constraints.append(articleImage.widthAnchor.constraint(equalTo: view.widthAnchor, constant: 70))
+        //constraints.append(articleImage.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10))
+        
+//        constraints.append(articleContent.leadingAnchor.constraint(
+//            equalTo: view.leadingAnchor, constant: 10))
+//        constraints.append(articleContent.trailingAnchor.constraint(
+//            equalTo: view.trailingAnchor, constant: -10))
+       
+
+        NSLayoutConstraint.activate(constraints)
+        
+    }
         
     
     private let tableController = ArticleTableViewController()
@@ -56,7 +98,7 @@ class ArticleDetailViewController: UIViewController, MenuControllerDelegate, Det
         articleInfo.text = "Author: \(article.author), Date: \(dateFormatter.string(from: article.date))"
         
         articleImage.image = article.image
-//        articleImage?.frame = CGRect(x: 47, y: 191, width: 140, height: 214)
+        //articleImage.frame = CGRect(x: 47, y: 191, width: 140, height: 214)
 //        articleContent?.frame =  CGRect(x: 47, y: 191, width: 330, height: 640)
 //        articleContent!.textContainer.exclusionPaths = [UIBezierPath(rect: articleImage!.frame)]
         
@@ -68,11 +110,11 @@ class ArticleDetailViewController: UIViewController, MenuControllerDelegate, Det
         sideMenu?.leftSide = false
         SideMenuManager.default.rightMenuNavigationController = sideMenu
         SideMenuManager.default.addPanGestureToPresent(toView: view)
-        
+        addLabels()
     }
     
     
-
+    
     @IBAction func didTabMenuButton(){
         present(sideMenu!, animated: true)
     }
